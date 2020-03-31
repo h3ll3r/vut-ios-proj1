8c291077aa3ecb089502c0a1e604add7625aa4f6
Andrei Roshka <mjc.lambert.sn@gmail.com>
2020-03-31 22:50:12 +0200

    Success

--- test.sh
+++ test.sh
@@ -2,13 +2,10 @@
 
 # set -x
 # set -v
-IGNORE_FILENAMES=""
-FILE_ERE="^\..{1,2}"
-for FILE in $(find . \( -type f -o -type d \) 2>/dev/null); do
-    FILE=${FILE##*/}
-    if echo $FILE | egrep $FILE_ERE >/dev/null; then
-        IGNORE_FILENAMES=$FILE:$IGNORE_FILENAMES
-    fi
+str="/Users/study/documents/1BIT_SS/proj"
+IFS=/
+for SEGMENT in $str; do
+    echo "SEG:${SEGMENT}|"
 done
+echo $inc
 
-echo "Ignore: $IGNORE_FILENAMES"
